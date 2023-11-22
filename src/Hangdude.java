59c530e8c1cc122d4f95badb1686a8fd36484f79
kewb <44578042+kewb@users.noreply.github.com>
2023-11-22 17:07:07 +0200

    Add an API which returns a random word

--- src/Hangdude.java
+++ src/Hangdude.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
-import java.net.HttpURLConnection;
 import java.util.Arrays;
 import java.util.Random;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Hangdude {
 
@@ -18,64 +19,96 @@ public class Hangdude {
     }
 
     public static void main(String[] args) {
-        
+        try {
+            Scanner scanner = new Scanner(System.in);
+            Random rand = new Random();
 
-        Scanner scanner = new Scanner(System.in);
-        Random rand = new Random();
-        String[] cars = { "Volvo", "Ford", "Mazda", "Maserati", "Mercedes-Benz", "Chevrolet", "Lamborghini", "Bugatti",
-                "Ferrari", "Bentley", "Aston Martin", "Land Rover", "Jaguar", "Tesla" };
+            String apiUrl = "https://random-word-api.herokuapp.com/word";
 
-        int randomNmb = rand.nextInt(cars.length);
-        char[] charArray = cars[randomNmb].toCharArray();
-        int length = charArray.length;
+            // Create a URL object
+            URL url = new URL(apiUrl);
 
-        System.out.println(cars[randomNmb]);
+            // Open a connection to the URL
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        int tries = 0;
-        int max_tries = 7;
-        char[] newArr = new char[length];
+            // Set the request method to GET
+            connection.setRequestMethod("GET");
 
-        for (int i = 0; i < length; i++) {
-            newArr[i] = '-';
-        }
-        while (!Arrays.equals(newArr, charArray) && max_tries > 0) {
+            // Get the response code
+            int responseCode = connection.getResponseCode();
 
-            System.out.println("Enter a letter:");
-            char letter = scanner.nextLine().charAt(0);
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Read the response from the API
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String response = reader.readLine();
 
+                // Use a regular expression to extract the word from the response
+                String regex = "\\[\"([^\"]*)\"\\]";
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(response);
 
-            boolean found = false;
-            for (int i = 0; i < charArray.length; i++) {
-                if (charArray[i] == letter && !found) {
-                    System.out.println(letter + " is at " + i);
-                    found = true;
-                    System.out.println(max_tries);
+                if (matcher.find()) {
+                    String randomWord = matcher.group(1);
+                    System.out.println("Random Word: " + randomWord);
+
+                    int length = randomWord.length();
+                    char[] charArray = randomWord.toCharArray();
+
+                    int tries = 0;
+                    int maxTries = 7;
+                    char[] newArr = new char[length];
+
+                    for (int i = 0; i < length; i++) {
+                        newArr[i] = '-';
+                    }
+
+                    while (!Arrays.equals(newArr, charArray) && maxTries > 0) {
+                        System.out.println("Enter a letter:");
+                        char letter = scanner.nextLine().charAt(0);
+
+                        boolean found = false;
+                        for (int i = 0; i < charArray.length; i++) {
+                            if (charArray[i] == letter && !found) {
+                                System.out.println(letter + " is at " + i);
+                                found = true;
+                                System.out.println(maxTries);
+                            }
+                        }
+
+                        for (int i = 0; i < length; i++) {
+                            if (charArray[i] == letter) {
+                                newArr[i] = letter;
+                            }
+                        }
+
+                        for (char item : newArr) {
+                            System.out.print(item + " ");
+                        }
+                        System.out.println();
+
+                        if (!found) {
+                            maxTries--;
+                            System.out.println("Try again, lives left: " + maxTries);
+                        }
+                    }
+
+                    if (Arrays.equals(newArr, charArray)) {
+                        System.out.println("Congrats, you survived");
+                    } else {
+                        System.out.println("You died");
+                    }
+                } else {
+                    System.out.println("Failed to extract the word from the response.");
                 }
+            } else {
+                System.out.println("Failed to fetch random word. Response Code: " + responseCode);
             }
 
-            for (int i = 0; i < length; i++) {
-                if (charArray[i] == letter) {
-                    newArr[i] = letter;
-                }
-            }
+            // Close the connection
+            connection.disconnect();
 
-            for (char item : newArr) {
-                System.out.print(item + "");
-            }
-            System.out.println();
-
-            if (!found) {
-                max_tries = max_tries - 1;
-
-                System.out.println("Try again, lives left: " + max_tries);
-
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        if (Arrays.equals(newArr, charArray)) {
-            System.out.println("Congrats, you survived");
-        } else {
-            System.out.println("You died");
-        }
-
     }
 }
